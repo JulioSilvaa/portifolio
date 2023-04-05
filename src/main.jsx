adb709428975469cbcd9340e3694bbc712991c4f
julio cesar da silva <juliocesar.amancio@yahoo.com.br>
2023-04-05 15:08:06 -0300

    feat: add theme color in main

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
 import App from './App';
+import theme from './style/ColorTheme';
 import GlobalStyles from './style/Global';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <GlobalStyles />
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
