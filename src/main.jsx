c0fd8eaad2507bb83b62cd36365958e281fded1c
julio cesar da silva <juliocesar.amancio@yahoo.com.br>
2023-04-05 16:02:05 -0300

    feat: add router from home in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
-import Home from './pages/Home';
+import Router from './routes/Router';
 import theme from './style/ColorTheme';
 import GlobalStyles from './style/Global';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <Router />
       <GlobalStyles />
-      <Home />
     </ThemeProvider>
   </React.StrictMode>,
 );
---
+++ src/routes/Router.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+//Pages
+import Home from '../pages/Home';
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
