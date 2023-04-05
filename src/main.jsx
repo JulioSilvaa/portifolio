d01522636a3931d16f6ead333cf8cff1fc20cc1b
julio cesar da silva <juliocesar.amancio@yahoo.com.br>
2023-04-05 15:53:12 -0300

    criando Home/page

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
-import App from './App';
+import Home from './pages/Home';
 import theme from './style/ColorTheme';
 import GlobalStyles from './style/Global';
 
@@ -9,7 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <App />
+      <Home />
     </ThemeProvider>
   </React.StrictMode>,
 );
