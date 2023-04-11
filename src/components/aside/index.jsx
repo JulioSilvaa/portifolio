0cd781fc899af0240497b2d91f313accaa506f49
julio cesar da silva <juliocesar.amancio@yahoo.com.br>
2023-04-11 18:08:09 -0300

    atualizando path imagem perfil

--- src/components/aside/index.jsx
+++ src/components/aside/index.jsx
@@ -5,7 +5,7 @@ import * as S from './style.aside';
 export default function Aside() {
   return (
     <S.Aside>
-      <S.Avatar src="../../../public/assets/profile-pic (3).png" alt="foto do meu perfil" />
+      <S.Avatar src="/assets/profile-pic (3).png" alt="foto do meu perfil" />
       <S.Title>Desenvolvedor</S.Title>
       <SocialMidia />
       <Contact />
